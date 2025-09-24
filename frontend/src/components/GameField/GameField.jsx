9c20da149271a6b04fd107dbeeba15d1543e80bf
kiteOFyte <keruells@gmail.com>
2025-09-24 21:06:16 +0300

    feature(structure): Построена структура поля в клетку #9

---
+++ frontend/src/components/GameField/GameField.jsx
@@ -0,0 +1,11 @@
+import './GameField.css'
+
+export default function GameField() {
+    return(
+        <div className="game-field">
+            <div className="game-field__grid">
+                
+            </div>
+        </div>
+    );
+}
