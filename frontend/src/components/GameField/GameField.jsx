edd3b4fe6f79b586e5ef29d6125db29210ee635a
kiteOFyte <keruells@gmail.com>
2025-09-25 23:16:38 +0300

    feature(structure): Сформирована тестовая логика движения кота вниз по нажатию на клавишу s #11

--- frontend/src/components/GameField/GameField.jsx
+++ frontend/src/components/GameField/GameField.jsx
@@ -2,10 +2,13 @@ import './GameField.css'
 import Cat from '../Cat/Cat';
 
 export default function GameField() {
-    return(
+    return (
         <div className="game-field">
             <div className="game-field__grid">
-                <Cat></Cat>
+                <Cat
+                    x="50%"
+                    y="30px"
+                />
             </div>
         </div>
     );
