72aa5f48c26f8a578579ca193ef872273a882a59
kiteOFyte <keruells@gmail.com>
2025-09-26 18:54:43 +0300

    feature(structure): Добавлен клон компонента Cat: СatV2 #13

---
+++ frontend/src/components/CatV2/CatV2.jsx
@@ -0,0 +1,42 @@
+import { useEffect, useRef } from 'react';
+import classes from './CatV2.module.css';
+
+
+export default function CatV2({ x, y }) {
+
+    const cat = useRef(null);
+
+    useEffect(() => {
+        let moveDown = null;
+        const handleKeyDown = (e) => {
+            if (e.key === "s" && !e.repeat) {
+                moveDown = setInterval(() => {
+                    cat.current.style.top = parseInt(cat.current.style.top) + 1 + "px";
+                }, 30)
+                cat.current.classList.add(classes.walkDown);
+            }
+        };
+
+        const handleKeyUp = (e) => {
+            clearInterval(moveDown);
+            cat.current.classList.remove(classes.walkDown);
+        };
+
+        window.addEventListener("keydown", handleKeyDown);
+        window.addEventListener("keyup", handleKeyUp);
+
+
+        return () => {
+            clearInterval(moveDown);
+            window.removeEventListener("keydown", handleKeyDown);
+            window.removeEventListener("keyup", handleKeyUp);
+        };
+
+    }, [])
+
+    return (
+        <div ref={cat} className={[classes.cat].join(" ")} style={{ top: y, left: x }}>
+
+        </div>
+    )
+}
--- frontend/src/components/GameField/GameField.jsx
+++ frontend/src/components/GameField/GameField.jsx
@@ -1,11 +1,12 @@
 import './GameField.css'
 import Cat from '../Cat/Cat';
+import CatV2 from '../CatV2/CatV2';
 
 export default function GameField() {
     return (
         <div className="game-field">
             <div className="game-field__grid">
-                <Cat
+                <CatV2
                     x="50%"
                     y="30px"
                 />
