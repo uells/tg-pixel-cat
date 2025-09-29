cedf50e980b21923451ef9e3656b5bc93cb2d021
kiteOFyte <keruells@gmail.com>
2025-09-29 16:16:02 +0300

    feature(structure): Создана механика для расчета размера поля #15

--- frontend/src/components/GameField/GameField.jsx
+++ frontend/src/components/GameField/GameField.jsx
@@ -1,15 +1,31 @@
+import { useRef, useEffect, useState } from "react"
 import './GameField.css'
 import Cat from '../Cat/Cat';
-import CatV2 from '../CatV2/CatV2';
 
 export default function GameField() {
+    const fieldGrid = useRef();
+    const [fieldSize, setFieldSize] = useState();
+
+    useEffect(() => {
+        if (fieldGrid.current) {
+            setFieldSize({
+                width: fieldGrid.current.offsetWidth,
+                height: fieldGrid.current.offsetHeight,
+            });
+        }
+    }, []);
+
     return (
         <div className="game-field">
-            <div className="game-field__grid">
-                <CatV2
-                    x="450px"
-                    y="30px"
-                />
+            <div ref={fieldGrid} className="game-field__grid">
+                {fieldSize && (
+                    <Cat
+                        x={fieldSize.width / 2}
+                        y={fieldSize.height / 2}
+                        fieldSize = {fieldSize}
+                    />
+                )}
+
             </div>
         </div>
     );
